8174fdee5c9829e3b6a2837a8485664773af97d0
Ur-Code-Buddy <05baivab@gmail.com>
2024-08-10 14:22:03 +0530

    added more pictures

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -17,19 +17,22 @@ const Explore: React.FC = () => {
   const { colorMode } = useColorMode();
 
   const images = [
-    'https://images.unsplash.com/photo-1517330357046-3ab5a5dd42a1?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1467307983825-619715426c70?q=80&w=1977&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1524613032530-449a5d94c285?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://www.wanderershub.com/wp-content/uploads/2023/09/Cafe-des-Art-Puducherry.jpg',
+    'https://www.wanderershub.com/wp-content/uploads/2023/09/PONDICHERRY-ATTRACTIONS-1536x917.png',
+    'https://tripnxt.com/blog/wp-content/uploads/2020/05/paradise-beach-tn.jpg',
     'https://images.unsplash.com/photo-1515091943-9d5c0ad475af?q=80&w=1535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1588623967205-d31b760a32bc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1710006937452-8548a7e1686b?q=80&w=1987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1710046385390-d8f68517c21b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://upload.wikimedia.org/wikipedia/commons/0/0b/A_night_in_the_streets_of_Pondicherry%21.jpg',
+    'https://static2.tripoto.com/media/filter/tst/img/294132/TripDocument/1633157080_img_20210820_131426.jpg',
     'https://images.unsplash.com/photo-1566224052100-7dfb53549de6?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://res.cloudinary.com/purnesh/image/upload/f_auto/v1516609214/cafe108.jpg',
     'https://images.unsplash.com/photo-1709623244505-da3f9ea63984?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHVkdWNoZXJyeXxlbnwwfHwwfHx8MA%3D%3D',
     'https://images.unsplash.com/photo-1709805471116-26c5adf3012b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHVkdWNoZXJyeXxlbnwwfHwwfHx8MA%3D%3D',
     'https://images.unsplash.com/photo-1710005556849-1a8b237a8eb7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHVkdWNoZXJyeXxlbnwwfHwwfHx8MA%3D%3D',
     'https://images.unsplash.com/photo-1566303052303-b2d2a9f16f0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHVkdWNoZXJyeXxlbnwwfHwwfHx8MA%3D%3D',
     'https://images.unsplash.com/photo-1709625215458-69e36584c9f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHVkdWNoZXJyeXxlbnwwfHwwfHx8MA%3D%3D',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCu-dyo6Tj2hYJ9p0LTtROBTQxIqrdxrN1Pg&s',
     'https://images.unsplash.com/photo-1662572594616-c5752f12dc10?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHB1ZHVjaGVycnl8ZW58MHx8MHx8fDA%3D',
     'https://images.unsplash.com/photo-1581791535045-5819f06be99b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHB1ZHVjaGVycnl8ZW58MHx8MHx8fDA%3D',
     'https://images.unsplash.com/photo-1619500178934-aa67d4c6623b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXVyb3ZpbGxlfGVufDB8fDB8fHww',
@@ -44,6 +47,17 @@ const Explore: React.FC = () => {
     'https://imgs.search.brave.com/wMkX7RXzJiNqEoUNvCLCzCLX_cRyxDz5NCZRzjczwNo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9i/L2I3L1BvbmRpY2hl/cnJ5LF9Qcm9tZW5h/ZGVfQmVhY2gsX0Jh/eV9vZl9CZW5nYWws/X0luZGlhLmpwZw',
     'https://imgs.search.brave.com/qCev5plByjn9ZvnhQGiOozOGSOmJSvBVC_P1zKxvEQU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMyLnRyaXBvdG8u/Y29tL21lZGlhL2Zp/bHRlci90c3QvaW1n/LzEyNTEwNzUvVHJp/cERvY3VtZW50LzE2/MjM1MjUzNTNfMF8x/ZXI5ZmVld3BhcWx0/YWswLmpwZy53ZWJw',
     'https://imgs.search.brave.com/hnsi6d3poJqTVh0eOwiMkZwT8lk2ZV4gxgq6mJ8Hs-A/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy84/LzhjL1BvbmRpY2hl/cnJ5LVJvY2tfYmVh/Y2hfYWVyaWFsX3Zp/ZXcuanBn',
+    'https://img.veenaworld.com/wp-content/uploads/2023/03/10-Things-That-Make-Pondicherry-An-Unforgetful-Travel-Destination.jpg',
+    'http://www.theuntourists.com/wp-content/uploads/2015/11/DSCF41881.jpg',
+    'https://static2.tripoto.com/media/filter/nl/img/187611/TripDocument/1493120153_promenade_beach_pondicherry.jpg',
+    'https://static2.tripoto.com/media/filter/nl/img/187611/TripDocument/1493120381_travels_pondicherry.png',
+    'https://i.pinimg.com/736x/4c/ea/6c/4cea6caeece6f3ae6ae0920f03d31bcb.jpg',
+    'https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180221130258/iStock-500320921-1024x682.jpg',
+    'https://www.seawatersports.com/img/newimage/places/white-town--.jpg',
+    'https://i0.wp.com/indianvagabond.com/wp-content/uploads/2016/09/pondicherry-white-town-6.jpg?resize=716%2C484&ssl=1',
+    'https://revolvingcompass.com/wp/wp-content/uploads/2022/09/WhatsApp-Image-2022-08-16-at-10.11.59-PM-1-1024x768.jpeg?x42979',
+    'https://static2.tripoto.com/media/filter/tst/img/183810/TripDocument/1440998902_img_20141113_063611712_hdr.jpg',
+
 
   ]
 
@@ -95,10 +109,13 @@ const Explore: React.FC = () => {
         bg="white"
         maxW="100%"
         mx="auto"
-        bgGradient="linear(to-r, #f0f4f8, white, #f0f4f8)"
+        bgImage="url('https://assets.traveltriangle.com/blog/wp-content/uploads/2017/05/auroville-beach-kb-920506.jpg')"
+        bgSize="cover"
+        bgPosition="center"
+        bgAttachment="fixed"  /* Added this property */
       >
         <Box
-          bgGradient="linear(to-r, teal.50, teal.100, teal.50)"
+          bgGradient="linear(to-r, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8))"
           maxW="1200px"
           mx="auto"
           borderRadius="lg"
@@ -121,7 +138,7 @@ const Explore: React.FC = () => {
             gridAutoRows="10px"
             gap={6}
             p={4}
-            bgGradient="linear(to-b, white, teal.50)"
+            bgGradient="linear(to-b, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))"
             borderRadius="lg"
             boxShadow="xl"
           >
