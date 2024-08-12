461cb8a8670a4891c0fef6eccf1460b0f90c35c9
Ur-Code-Buddy <05baivab@gmail.com>
2024-08-12 23:10:25 +0530

    conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/image" element={<ImagePage/>} />
         <Route path="/form" element={<FormPage/>} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/login" element={<LoginPanel/>} />
         <Route path="/adminpanel" element={<AdminPanel />} />
+        <Route path="/image" element={<ImagePage/>} />
 
         <Route path="*" element={<Homepage />} />
         
